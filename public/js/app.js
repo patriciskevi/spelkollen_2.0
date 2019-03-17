43e867c426169de512f87cbd22584fafaec0c7ec
Patric Iskevi <patriciskevi@gmail.com>
2019-03-17 12:43:05 +0100

    testing firebase realtime database

--- public/js/app.js
+++ public/js/app.js
@@ -2,72 +2,69 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD81EtdnxKg8C2-1VhVB7QQCbps9mBYgFY",
-    authDomain: "spelkollen-ea7cc.firebaseapp.com",
-    databaseURL: "https://spelkollen-ea7cc.firebaseio.com",
-    projectId: "spelkollen-ea7cc",
-    storageBucket: "spelkollen-ea7cc.appspot.com",
-    messagingSenderId: "744517835900"
+  apiKey: "AIzaSyD81EtdnxKg8C2-1VhVB7QQCbps9mBYgFY",
+  authDomain: "spelkollen-ea7cc.firebaseapp.com",
+  databaseURL: "https://spelkollen-ea7cc.firebaseio.com",
+  projectId: "spelkollen-ea7cc",
+  storageBucket: "spelkollen-ea7cc.appspot.com",
+  messagingSenderId: "744517835900"
 };
 firebase.initializeApp(config);
 
-
-
 // GOOGLE LOGIN
 function googleLogin() {
-    function newLogin(user) {
-        if (user) {
-            window.location = 'home.html';
-        } else {
-            const provider = new firebase.auth.GoogleAuthProvider();
-            const auth = firebase.auth().signInWithPopup(provider);
-        }
+  function newLogin(user) {
+    if (user) {
+      window.location = "home.html";
+    } else {
+      const provider = new firebase.auth.GoogleAuthProvider();
+      const auth = firebase.auth().signInWithPopup(provider);
     }
+  }
 
-    firebase.auth().onAuthStateChanged(newLogin);
+  firebase.auth().onAuthStateChanged(newLogin);
 }
 
 // FACEBOOK LOGIN
 function facebookLogin() {
-    function newLogin(user) {
-        if (user) {
-            window.location = 'home.html';
-            app(user);
-        } else {
-            const provider = new firebase.auth.FacebookAuthProvider();
-            const auth = firebase.auth().signInWithPopup(provider);
-        }
+  function newLogin(user) {
+    if (user) {
+      window.location = "home.html";
+      app(user);
+    } else {
+      const provider = new firebase.auth.FacebookAuthProvider();
+      const auth = firebase.auth().signInWithPopup(provider);
     }
+  }
 
-    firebase.auth().onAuthStateChanged(newLogin);
+  firebase.auth().onAuthStateChanged(newLogin);
 }
 
 function logout() {
-    firebase.auth().signOut();
-    window.location = 'index.html';
+  firebase.auth().signOut();
+  window.location = "index.html";
 }
 // END OF LOGIN
 
-
 let bets = [];
 
-function getLocalStorage() {
-    if (localStorage.getItem('bets') === null) {
-        bets = [];
-    } else {
-        bets = JSON.parse(localStorage.getItem('bets'));
-    }
-}
+// function getLocalStorage() {
+//   if (localStorage.getItem("bets") === null) {
+//     bets = [];
+//   } else {
+//     bets = JSON.parse(localStorage.getItem("bets"));
+//   }
+// }
 
-function setLocalStorage() {
-    localStorage.setItem('bets', JSON.stringify(bets));
-}
+// function setLocalStorage() {
+//   localStorage.setItem("bets", JSON.stringify(bets));
+// }
 
 function renderBetCard() {
-    const card = document.createElement('div');
-    document.querySelector('#card-bet').innerHTML = '';
-    for (let bet of bets) {
-        card.innerHTML = `
+  const card = document.createElement("div");
+  document.querySelector("#card-bet").innerHTML = "";
+  for (let bet of bets) {
+    card.innerHTML = `
         <div class="col s12">
             <div class="card horizontal">
                 <div class="card-stacked">
@@ -79,11 +76,9 @@ function renderBetCard() {
                         <p>Vinst: ${bet.win}:-</p> 
                     </div>
                     <div class="card-action">
-                    <a href="#" id="${
-                        bet.id
-                      }" onclick="betRemove(${
-                        bet.id
-                      })"><i class="material-icons card-action-icon">delete</i></a>
+                    <a href="#" id="${bet.id}" onclick="betRemove(${
+      bet.id
+    })"><i class="material-icons card-action-icon">delete</i></a>
                     <a href="#" onclick="editCard()"><i class="material-icons card-action-icon">
                             create
                         </i></a>
@@ -92,23 +87,21 @@ function renderBetCard() {
             </div>
         </div>
         `;
-        document.querySelector('#card-bet').innerHTML += card.innerHTML;
-    }
-
+    document.querySelector("#card-bet").innerHTML += card.innerHTML;
+  }
 }
 
 function editCard() {
-    // const card = document.createElement('div');
-    // const flip = document.querySelector('.card');
-    // flip.onclick = function () {
-    //     flip.classList.add('flip-vertical-fwd', 'card-back');
-    // };
-    console.log('edit');
+  // const card = document.createElement('div');
+  // const flip = document.querySelector('.card');
+  // flip.onclick = function () {
+  //     flip.classList.add('flip-vertical-fwd', 'card-back');
+  // };
+  console.log("edit");
 }
 
 function renderAddBetCard() {
-
-    document.querySelector('.add-bet-card').innerHTML = `
+  document.querySelector(".add-bet-card").innerHTML = `
     <div id="add-bet-card">
     <div class="col s12">
         <div class="card horizontal add-bet-card">
@@ -140,42 +133,49 @@ function renderAddBetCard() {
     </div>    
     </div>
     `;
-
 }
 
 function playerCard() {
-    firebase.auth().onAuthStateChanged(function (user) {
-        for (let player of getPlayersTotalBets()) {
-            const card = document.createElement('div'); {
-                card.innerHTML = `
+  firebase.auth().onAuthStateChanged(function(user) {
+    for (let player of getPlayersTotalBets()) {
+      const card = document.createElement("div");
+      {
+        card.innerHTML = `
                             <div class="col s12">
                                 <div class="card horizontal">
                                     <div class="card-stacked">
                                         <div class="card-content">
-                                            <img id="cardUserPhoto" src="${user.photoURL}">
+                                            <img id="cardUserPhoto" src="${
+                                              user.photoURL
+                                            }">
                                             <p><b>${user.displayName}</b>,</p>
-                                            <p>Du har spelat för: ${player.sum}</p>
-                                            <p>Din totala vinstsumma är: ${player.win}</p>
+                                            <p>Du har spelat för: ${
+                                              player.sum
+                                            }</p>
+                                            <p>Din totala vinstsumma är: ${
+                                              player.win
+                                            }</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             `;
-                document.querySelector('#stats').innerHTML = card.innerHTML;
-            }
-        }
-    });
+        document.querySelector("#stats").innerHTML = card.innerHTML;
+      }
+    }
+  });
 }
 
 function totalCard() {
-    // const sum = bets.reduce(add);
+  // const sum = bets.reduce(add);
 
-    // function add(accumulator, a) {
-    //     return accumulator + a;
-    // }
+  // function add(accumulator, a) {
+  //     return accumulator + a;
+  // }
 
-    const card = document.createElement('div'); {
-        card.innerHTML = `
+  const card = document.createElement("div");
+  {
+    card.innerHTML = `
                             <div class="col s12">
                                 <div class="card horizontal">
                                     <div class="card-stacked">
@@ -187,122 +187,117 @@ function totalCard() {
                                 </div>
                             </div>
                             `;
-        document.querySelector('#stats').innerHTML = card.innerHTML;
-    }
+    document.querySelector("#stats").innerHTML = card.innerHTML;
+  }
 }
 
 function addButton() {
-    const overlay = document.querySelector('.main');
-    renderAddBetCard();
+  const overlay = document.querySelector(".main");
+  renderAddBetCard();
 
-    // addButton.onclick = renderAddBetCard();
-    overlay.classList.toggle('overlay');
+  // addButton.onclick = renderAddBetCard();
+  overlay.classList.toggle("overlay");
 }
 
 function betAdd() {
-    const name = document.querySelector('#name').value;
-    const sum = document.querySelector('#sum').value;
-    const win = document.querySelector('#win').value;
-    const exit = document.querySelector('#add-bet-card');
-    const overlay = document.querySelector('.main');
+  const name = document.querySelector("#name").value;
+  const sum = document.querySelector("#sum").value;
+  const win = document.querySelector("#win").value;
+  const exit = document.querySelector("#add-bet-card");
+  const overlay = document.querySelector(".main");
 
-    const date = new Date();
+  const date = new Date();
 
-    bets.push({
-        name,
-        date: `${date.getFullYear()}/${date.getMonth() +1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-        sum: parseInt(sum),
-        win: parseInt(win),
-        id: bets.length + 1
-    });
-    exit.onclick = function () {
-        exit.parentNode.removeChild(exit);
-        overlay.classList.toggle('overlay');
-    };
-    renderApp();
-    playerCard();
-    totalCard();
+  bets.push({
+    name,
+    date: `${date.getFullYear()}/${date.getMonth() +
+      1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
+    sum: parseInt(sum),
+    win: parseInt(win),
+    id: bets.length + 1
+  });
+  exit.onclick = function() {
+    exit.parentNode.removeChild(exit);
+    overlay.classList.toggle("overlay");
+  };
+  renderApp();
+  playerCard();
+  totalCard();
 }
 
 function betRemove(id) {
-    bets = bets.filter(bet => {
-        return bet.id != id;
-    });
-    renderApp();
+  bets = bets.filter(bet => {
+    return bet.id != id;
+  });
+  renderApp();
 }
 
 function getPlayersTotalBets() {
-    const players = [];
-    for (let bet of bets) {
-        const player = players.find(player => player.name === bet.name);
-        if (!player) {
-            players.push({
-                name: bet.name,
-                win: bet.win,
-                sum: bet.sum,
-                id: bet.id,
-                date: bet.date
-            });
-        } else {
-            player.win += bet.win;
-            player.sum += bet.sum;
-        }
+  const players = [];
+  for (let bet of bets) {
+    const player = players.find(player => player.name === bet.name);
+    if (!player) {
+      players.push({
+        name: bet.name,
+        win: bet.win,
+        sum: bet.sum,
+        id: bet.id,
+        date: bet.date
+      });
+    } else {
+      player.win += bet.win;
+      player.sum += bet.sum;
     }
-    return players;
+  }
+  return players;
 }
-
-
-
 
 function renderApp() {
-    setLocalStorage();
+  //   setLocalStorage();
 
-    renderBetCard();
-    playerCard();
-    totalCard();
+  renderBetCard();
+  playerCard();
+  totalCard();
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    getLocalStorage();
-    renderApp();
+document.addEventListener("DOMContentLoaded", () => {
+  //   getLocalStorage();
+  renderApp();
 });
 
+const ul = document.querySelector("#display-text");
+const dbRefObject = firebase
+  .database()
+  .ref()
+  .child("bets");
 
-
-
-
-
-
-// if (bets[i].win > 0) {
-//     `<span class="circle-win"></span>`
-// } else {
-//     `<span class="circle-loss"></span>`
-// }
-
-
-
-
-// function getLocalStorage() {
-//     let bets = [];
-
-//     if (localStorage.getItem('bets') === null) {
-//         bets = [];
-//     } else {
-//         bets = JSON.parse(localStorage.getItem('bets'));
-//     }
-//     bets.forEach(bet =>
-//         this.bets.push({
-//             name: bet.name,
-//             date: `${date.getFullYear()}/${date.getMonth() +1}/${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`,
-//             sum: parseInt(bet.sum),
-//             win: parseInt(bet.win)
-//         })
-//     );
-
-//     localStorage.setItem('bets', JSON.stringify(bet));
-//     console.log(bets)
-// }
-
-// getLocalStorage();
-// renderBetCard();
+dbRefObject.on("value", snap => {
+  ul.innerHTML = " ";
+  snap.forEach(childSnap => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <div class="col s12">
+        <div class="card horizontal">
+            <div class="card-stacked">
+                <div class="card-content">
+                    <span class="circle"></span>
+                    <p><b>${childSnap.val().name}</b></p>
+                    <p></p>
+                    <p>Summa: ${childSnap.val().sum}:-</p>
+                    <p>Vinst: ${childSnap.val().win}:-</p> 
+                </div>
+                <div class="card-action">
+                <a href="#" id="${childSnap.key}" onclick="betRemove(${
+      childSnap.key
+    })"><i class="material-icons card-action-icon">delete</i></a>
+                <a href="#" onclick="editCard()"><i class="material-icons card-action-icon">
+                        create
+                    </i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    document.querySelector("#card-bet").innerHTML += li.innerHTML;
+  });
+});
